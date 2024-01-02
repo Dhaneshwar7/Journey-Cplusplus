8408c0be399d7be5f71d93241f2e7eb33fb9c893
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-01-02 06:18:57 +0000

    condition check

---
+++ BasicsProgramming-I-C++/Coditionals/coditionals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int score;
+    cout << "Enter Score" <<endl;
+    cin>> score;
+    if(score <300){
+        cout << "INdia Wins" <<endl;
+    }
+    else{
+    cout<< "Austrialia Wins " <<endl;
+    }
+
+}
