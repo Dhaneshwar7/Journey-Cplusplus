92dd65d451b80b7a55b28836c6db05019afca0f5
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-06-07 15:20:16 +0000

    pyramid

--- 03_Patterns/01_NumericFullPyramid.cpp
+++ 03_Patterns/01_NumericFullPyramid.cpp
@@ -6,20 +6,43 @@ int main(){
     cout<<"Enter Rows number";
     cin>> n;
 
-    for( int row=0; row<n;row=row+1){
-
-        //row + 1 elements print karne wale  hai
-        // int start = row+1;
-        for (int col=0; col<row+1; col=col+1){
-            //1st way
-            // cout << start << " ";
-            // start = start +1 ;
+    //First Half Part 
+    // for( int row=0; row<n;row=row+1){
+    //     //row + 1 elements print karne wale  hai
+    //     // int start = row+1;
+    //     for (int col=0; col<row+1; col=col+1){
+    //         //1st way
+    //         // cout << start << " ";
+    //         // start = start +1 ;
             
-            // 2way another formula
-            cout << row  +col +1 << " ";
+    //         // 2way another formula
+    //         cout << row  +col +1 << " ";
+    //     }
+    //     cout<<endl;
+    // } 
 
+    // Full Pyramid part --
+    for(int row =0; row <n ; row = row+1 ){
+        //spaces
+        for(int col=0; col< n-row-1;col=col+1){
+        cout << " ";
         }
-        cout<<endl;
+
+        //numbers
+        for(int col=0; col< row+1; col=col+1){
+            cout << row +col +1;
+        }
+        //reverse pyramid
+        int start = 2*row;
+        for(int col=0; col<row ;col=col+1){
+            cout<< start;
+            start =start -1;
+        }
+        cout << endl;
+
     }
+
+
+    
 
 }
---
+++ 03_Patterns/02_NumericHollowPyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout<< "Enter you Number for Hollow Pyaramid";
+
+    for (int row=0;row<n;row=row+1){
+        // spaces
+        for(int col=0;col<n-row-1;col=col+1){
+            cout<< " ";
+        }
+
+        // number with spaces in between
+        for(int col=0;col<)
+    }
+}
