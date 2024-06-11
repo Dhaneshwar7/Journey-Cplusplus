52ee5390d2b80f0ce1a2386c0b08cecc8c728228
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-06-11 18:55:30 +0000

    updated

---
+++ 03_Patterns/00_normal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a =5;
+    char b= 'd';
+
+    int sum = a+b;
+    cout<< sum<< endl;
+    float f= 2.1+ sum;
+    cout<< f<<endl;
+
+    float ff= 2.3;
+    int num= 157;
+    int final = num- ff;
+    cout<< final << endl;
+}
