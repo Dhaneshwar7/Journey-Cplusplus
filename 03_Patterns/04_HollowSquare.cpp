b6c345e5e6956617f5492c89683146c1e936d7a1
Dhaneshwar Marskole <87529812+Dhaneshwar7@users.noreply.github.com>
2024-06-14 11:08:20 +0000

    Hollow Sqaure Pattern printed

---
+++ 03_Patterns/04_HollowSquare.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+  
+ 
+int main(){
+    int n;
+    cout<< "Enter a Number for Solid Square Pattern"<< endl;
+    cin>>n;
+  
+  for(int row=0; row<n;row++){
+    for (int col=0; col<n;col++){
+        if(row==0 || row ==n-1 ||col==0|| col==n-1){
+            cout<< "* ";
+        }
+        else{
+        cout<< "  ";
+
+        }
+    }
+    cout<<endl;
+  }
+ }
